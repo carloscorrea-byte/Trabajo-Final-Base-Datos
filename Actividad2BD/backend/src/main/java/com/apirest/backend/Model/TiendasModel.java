592e4c344c72c81e04fe6324e1846fb149afc898
Soy Leidy <soyleidylainge@gmail.com>
2026-04-24 20:32:22 -0500

    Moviendo carpetas y clases
    Se realizaron algunos movimientos de carpetas y clases ya que estas se encuentran por fuera del mismo.

---
+++ Actividad2BD/backend/src/main/java/com/apirest/backend/Model/TiendasModel.java
@@ -0,0 +1,25 @@
+package com.apirest.backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document("Tiendas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TiendasModel {
+    @Id
+    private ObjectId id;
+    private String nombre;
+    private DireccionTienda direccion; // este atributo representa el documento embebido
+    private List<Integer> telefonos = new ArrayList<>();
+}
