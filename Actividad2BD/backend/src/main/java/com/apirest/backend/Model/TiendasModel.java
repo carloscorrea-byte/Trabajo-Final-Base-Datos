d03dc875d411d4df8128bbcf18337570d549b00d
Soy Leidy <soyleidylainge@gmail.com>
2026-04-26 13:13:14 -0500

    Modificando TiendasModel
    Se agregaron las lineas 10, 27-30 para que en postman en ves de mostrarnos la fecha y hora (date y timestamp), porque lo que queremos es que nos muestre el valor real “ObjectId” como se dejo estipulado en la BD de MongoDB.

--- Actividad2BD/backend/src/main/java/com/apirest/backend/Model/TiendasModel.java
+++ Actividad2BD/backend/src/main/java/com/apirest/backend/Model/TiendasModel.java
@@ -7,6 +7,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.Jsonproperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,11 +17,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class TiendasModel {
     @Id
     private ObjectId id;
     private String nombre;
     private DireccionTienda direccion; // este atributo representa el documento embebido
     private List<Integer> telefonos = new ArrayList<>();
+
+    @JsonProperty("id")
+    public String getIdAsString(){
+        return id != null ? id.toHexString() : null;
+    }
 }
