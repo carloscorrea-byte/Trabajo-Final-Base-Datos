3cc5fe47f7ef1ea1043df27a6ba8d5b9b2109171
Carlos Alberto Correa Gutierrez <carlos.correa@uao.edu.co>
2026-04-26 22:18:46 -0500

    Falla Tipografica Corregida
    Solucion Falla Tipografica:
    JsonProperty

--- Actividad2BD/backend/src/main/java/com/apirest/backend/Model/TiendasModel.java
+++ Actividad2BD/backend/src/main/java/com/apirest/backend/Model/TiendasModel.java
@@ -7,9 +7,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/* 
-import com.fasterxml.jackson.annotation.Jsonproperty;
-*/
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,11 +26,10 @@ public class TiendasModel {
     private DireccionTienda direccion; // este atributo representa el documento embebido
     private List<Integer> telefonos = new ArrayList<>();
 
-    /* 
+    
     @JsonProperty("id")
     public String getIdAsString(){
         return id != null ? id.toHexString() : null;
     }
-    */
-
+    
 }
